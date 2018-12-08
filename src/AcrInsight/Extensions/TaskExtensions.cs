c75ff166da2cb7342aed08c6859613d96329dd1e
Takaaki Suzuki <xin9le@live.jp>
2018-12-08 19:55:04 +0900

    Add extension method for Task.

---
+++ src/AcrInsight/Extensions/TaskExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+
+namespace AcrInsight.Extensions
+{
+    /// <summary>
+    /// Provides extension methods of <see cref="Task{T}"/>.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Creates a task that will complete when all of the <see cref="Task{T}"/> objects in an enumerable collection have completed.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tasks"></param>
+        /// <returns></returns>
+        public static Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks)
+        {
+            if (tasks == null)
+                throw new ArgumentNullException(nameof(tasks));
+            return Task.WhenAll(tasks);
+        }
+    }
+}
