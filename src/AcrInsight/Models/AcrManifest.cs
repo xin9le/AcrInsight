37ad45f1d74ccdaeb48eb21c26541313d19b108f
Takaaki Suzuki <xin9le@live.jp>
2018-12-08 20:13:05 +0900

    Implement model layer.

---
+++ src/AcrInsight/Models/AcrManifest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.Azure.ContainerRegistry.Models;
+
+
+
+namespace AcrInsight.Models
+{
+    /// <summary>
+    /// Represents a container image manifest.
+    /// </summary>
+    public class AcrManifest
+    {
+        #region Properties
+        /// <summary>
+        /// Gets digest (= image's hash ID).
+        /// </summary>
+        public string Digest { get; }
+
+
+        /// <summary>
+        /// Gets tags associated with.
+        /// </summary>
+        public IReadOnlyList<string> Tags { get; }
+
+
+        /// <summary>
+        /// Gets operating system.
+        /// </summary>
+        public string OperatingSystem { get; }
+
+
+        /// <summary>
+        /// Gets CPU architecture.
+        /// </summary>
+        public string Architecture { get; }
+
+
+        /// <summary>
+        /// Gets created time.
+        /// </summary>
+        public string CreatedTime { get; }
+
+
+        /// <summary>
+        /// Gets last update time.
+        /// </summary>
+        public string LastUpdateTime { get; }
+        #endregion
+
+
+        #region Constructor
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="source"></param>
+        public AcrManifest(AcrManifestAttributesBase source)
+        {
+            this.Digest = source.Digest;
+            this.Tags = new ReadOnlyCollection<string>(source.Tags);
+            this.OperatingSystem = source.Os;
+            this.Architecture = source.Architecture;
+            this.CreatedTime = source.CreatedTime;
+            this.LastUpdateTime = source.LastUpdateTime;
+        }
+        #endregion
+    }
+}
---
+++ src/AcrInsight/Models/AcrRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AcrInsight.Extensions;
+using Microsoft.Azure.ContainerRegistry;
+using Microsoft.Rest;
+
+
+
+namespace AcrInsight.Models
+{
+    /// <summary>
+    /// Represents a repository of Azure Container Registory.
+    /// </summary>
+    public class AcrRepository
+    {
+        #region Properties
+        /// <summary>
+        /// Gets repository name.
+        /// </summary>
+        public string Name { get; }
+
+
+        /// <summary>
+        /// Gets container image manifests.
+        /// </summary>
+        public AcrManifest[] Manifests { get; }
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="manifests"></param>
+        private AcrRepository(string name, AcrManifest[] manifests)
+        {
+            this.Name = name;
+            this.Manifests = manifests;
+        }
+        #endregion
+
+
+        #region Load
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <param name="loginServer"></param>
+        /// <returns></returns>
+        public static async ValueTask<AcrRepository[]> LoadAsync(string userName, string password, string loginServer)
+        {
+            if (userName == null) throw new ArgumentNullException(nameof(userName));
+            if (password == null) throw new ArgumentNullException(nameof(password));
+            if (loginServer == null) throw new ArgumentNullException(nameof(loginServer));
+
+            //--- create client
+            var credentials = new BasicAuthenticationCredentials
+            {
+                UserName = userName.Trim(),
+                Password = password.Trim(),
+            };
+            var client = new AzureContainerRegistryClient(credentials)
+            {
+                BaseUri = new Uri($"https://{loginServer.Trim()}"),
+            };
+
+            //--- load reposigories
+            var names = (await client.GetRepositoriesAsync().ConfigureAwait(false)).Names;
+            return (await names
+                .Select(async x =>
+                {
+                    var manifests
+                        = (await client.GetAcrManifestsAsync(x).ConfigureAwait(false))
+                        .Manifests
+                        .OrderByDescending(y => y.CreatedTime)
+                        .Select(y => new AcrManifest(y))
+                        .ToArray();
+                    return new AcrRepository(x, manifests);
+                })
+                .WhenAll()
+                .ConfigureAwait(false))
+                .OrderBy(x => x.Name)
+                .ToArray();
+        }
+        #endregion
+    }
+}
